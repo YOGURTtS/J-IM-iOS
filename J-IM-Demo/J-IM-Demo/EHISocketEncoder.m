939e8287e1911e46c87e6b0a6a23b29038eb8617
yogurts <scodield838727836@vip.qq.com>
2018-11-23 17:43:04 +0800

    Revert "消息模型改为一个"
    This reverts commit edbfa8e51aee65d4c2e3da06b80b278562a1c093.

--- J-IM-Demo/J-IM-Demo/EHISocketEncoder.m
+++ J-IM-Demo/J-IM-Demo/EHISocketEncoder.m
@@ -52,7 +52,7 @@
     }
     packetLength += 1 + 4 + bodyLength;
     
-    // 拼接data
+    NSLog(@"packetLength = %d", packetLength);
     NSMutableData *packetData = [NSMutableData data];
     [packetData appendBytes:&version length:1];
     
@@ -64,6 +64,7 @@
     [packetData appendBytes:&cmdByte length:1];
     [packetData appendBytes:&bodyLength length:4];
     [packetData appendData:bodyData];
+    NSLog(@"packetData = %@", packetData);
     
     return packetData;
 }
--- J-IM-Demo/J-IM-Demo/EHISocketMessage.h
+++ J-IM-Demo/J-IM-Demo/EHISocketMessage.h
@@ -13,31 +13,6 @@
 
 
 
-#pragma mark - 普通信息
-
-
-/**
- 消息类型
- */
-typedef NS_ENUM(int, EHISocketMessageType) {
-    EHISocketMessageTypeText    = 0,    /// 文字
-    EHISocketMessageTypeImage   = 1,    /// 图片
-    EHISocketMessageTypeVoice   = 2,    /// 语音
-    EHISocketMessageTypeVideo   = 3,    /// 视频
-    EHISocketMessageTypeMusic   = 4,    /// 音乐
-    EHISocketMessageTypeNews    = 5     /// 新闻
-};
-
-
-/**
- 聊天类型
- */
-typedef NS_ENUM(int, EHISocketChatType) {
-    EHISocketChatTypeUnknown    = 0,    /// 未知
-    EHISocketChatTypePublic     = 1,    /// 公聊
-    EHISocketChatTypePrivate    = 2,    /// 私聊
-};
-
 /**
  普通消息
  */
@@ -59,11 +34,11 @@ typedef NS_ENUM(int, EHISocketChatType) {
 /** 消息创建时间 */
 @property (nonatomic, assign) long createTime;
 
-/** 消息类型 (0:text、1:image、2:voice、3:vedio、4:music、5:news) */
-@property (nonatomic, assign) EHISocketMessageType msgType;
+/** 消息类型 */
+@property (nonatomic, assign) int msgType;
 
-/** 聊天类型 (0:未知,1:公聊,2:私聊) */
-@property (nonatomic, assign) EHISocketChatType chatType;
+/** 聊天类型 */
+@property (nonatomic, assign) int chatType;
 
 /** 内容 */
 @property (nonatomic, copy) NSString *content;
@@ -116,15 +91,6 @@ typedef NS_ENUM(int, EHISocketChatType) {
 
 
 
-#pragma mark - 关闭聊天信息
-/**
- 关闭类型
- */
-typedef NS_ENUM(NSUInteger, EHISocketCloseChatType) {
-    EHISocketCloseChatTypeByClient          = 1,    /// 客户关闭
-    EHISocketCloseChatTypeByCustomService   = 2,    /// 客服关闭
-};
-
 /**
  关闭聊天信息
  */
@@ -147,25 +113,11 @@ typedef NS_ENUM(NSUInteger, EHISocketCloseChatType) {
 @property (nonatomic, assign) long optTime;
 
 /** 类型 1 客户关闭 2 客服关闭 */
-@property (nonatomic, assign) EHISocketCloseChatType optType;
+@property (nonatomic, copy) NSString *optType;
 
 @end
 
 
-
-#pragma mark - 客服接受、拒绝、设置状态、最大接入人数、连接方式
-/**
- 客服状态
- */
-typedef NS_ENUM(NSUInteger, EHISocketCustomServiceOptType) {
-    EHISocketCustomServiceOptTypeAccept             = 1,    /// 接受
-    EHISocketCustomServiceOptTypeRefuse             = 2,    /// 拒绝
-    EHISocketCustomServiceOptTypeIdle               = 3,    /// 设置状态为空闲
-    EHISocketCustomServiceOptTypeBusy               = 4,    /// 设置状态为忙
-    EHISocketCustomServiceOptTypeMaxConnectNumber   = 5,    /// 设置最大接入数
-    EHISocketCustomServiceOptTypeConnectManual      = 6,    /// 设置连接方式为手动
-    EHISocketCustomServiceOptTypeAuto               = 7     /// 设置为自动
-};
 
 /**
  客服接受、拒绝、设置状态、最大接入人数、连接方式
@@ -188,7 +140,7 @@ typedef NS_ENUM(NSUInteger, EHISocketCustomServiceOptType) {
 @property (nonatomic, assign) long optTime;
 
 /** 类型 1 接受 2 拒绝 3设置状态为闲 4 设置状态为忙 5 设置最大接入数 6 设置连接方式为手动 7 设置为自动 */
-@property (nonatomic, assign) EHISocketCustomServiceOptType optType;
+@property (nonatomic, copy) NSString *optType;
 
 /** 最大连接人数 */
 @property (nonatomic, assign) int connectAmount;
